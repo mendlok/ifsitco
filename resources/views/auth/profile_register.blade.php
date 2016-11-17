855d9b5938d548b5d0b048bc76a5c3fc23e36e95
Johnny Rojas <Johnny Rojas>
2016-11-16 23:15:58 -0600

    Subo cambios en el name addres de profile register

--- resources/views/auth/profile_register.blade.php
+++ resources/views/auth/profile_register.blade.php
@@ -327,7 +327,7 @@
           <div class="form-group">
             <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12"  for="address">Dirección Exacta<span class="required">*</span></label>
             <div class="col-md-6 col-sm-6 col-xs-12">
-              <textarea id="address" class="form-control col-md-7 col-xs-12" name="addres" type="text"></textarea>
+              <textarea id="address" class="form-control col-md-7 col-xs-12" name="address" type="text"></textarea>
             </div>
           </div>
       </div>
