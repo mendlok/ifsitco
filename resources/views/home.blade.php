a6a4db45ea68566039addc2870017783123e703e
mendlok <johnnyzu963@gmail.com>
2016-11-17 12:01:11 -0600

    Subo cambios en los footer y los js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -148,12 +148,7 @@
     <!-- /page content -->
 
     <!-- footer content -->
-    <footer>
-        <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-        </div>
-        <div class="clearfix"></div>
-    </footer>
+   
 
     <script type="text/javascript">
       $(document).ready(function(){
--- resources/views/package/new_package.blade.php
+++ resources/views/package/new_package.blade.php
@@ -88,12 +88,7 @@
 </div>
 
 <!-- footer content -->
-<footer>
-    <div class="pull-right">
-        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-    </div>
-    <div class="clearfix"></div>
-</footer>
+
 <!-- /footer content -->
 
 <!-- /page content -->
