e36d03475e99edc06497b336a921684af387af56
mendlok <johnnyzu963@gmail.com>
2016-11-21 13:02:29 -0600

    Subo cambios en el footer de unas cosas y el title en layaouts

--- resources/views/package/pack_details.blade.php
+++ resources/views/package/pack_details.blade.php
@@ -265,12 +265,7 @@
 </div>
 
 <!-- footer content -->
-<footer>
-    <div class="pull-right">
-        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-    </div>
-    <div class="clearfix"></div>
-</footer>
+
 <!-- /footer content -->
 
 <!-- /page content -->
