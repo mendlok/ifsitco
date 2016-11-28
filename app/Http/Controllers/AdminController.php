525c63bebd0c4f0397baadbeef63d15a01e28b5c
mendlok <johnnyzu963@gmail.com>
2016-11-28 15:01:07 -0600

    subo controlador de home admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,4 +31,27 @@ class AdminController extends Controller
         $packages = Package::where('profile_id',Auth::user()->profile_id)->get();
         return view('Adminviews\PackageAdmin\Admin_package_alert',compact('packages'));
     }
+
+    public function newadmin()
+    {
+        $packages = Package::where('profile_id',Auth::user()->profile_id)->get();
+        return view('Adminviews\new_admin',compact('packages'));
+    }
+
+     public function serachprofileadmin()
+    {
+        $packages = Package::where('profile_id',Auth::user()->profile_id)->get();
+        return view('Adminviews\search_profile_admin',compact('packages'));
+    }
+
+     public function serachprofileclient()
+    {
+        $packages = Package::where('profile_id',Auth::user()->profile_id)->get();
+        return view('Adminviews\search_profile_client',compact('packages'));
+    }
+
+     public function adminhome()
+    {
+        return view('Adminviews\home_admin',compact('packages'));
+    }
 }
